0e8ede865bdeebf6e2aa60cdf892b238cd869c55
Preritdutta <116864212+Preritdutta@users.noreply.github.com>
2022-11-24 13:36:55 +0530

    Add files via upload

---
+++ checktriangle.js
@@ -0,0 +1,13 @@
+function chckTriangle(a,b,c){
+    
+     if(a==b && b==c)
+         console.log("triangle is equilaterl");
+      else
+      if(a==b||b==c||a==c)
+console.log("triangle is isoceles");
+           else
+console.log("triangle is scalene");
+
+}
+chckTriangle(9,2,9);
+
